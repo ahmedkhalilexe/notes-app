e6717a9098c91772a65ba871a28513d97f138f10
Ahmed Khalil AMRANI <amraniahmedkhalil@gmail.com>
2022-09-14 18:19:35 +0100

    changed adding note logic and added delete note

--- scripts/sripte.js
+++ scripts/sripte.js
@@ -6,21 +6,44 @@ const createbtn = document.getElementById("create");
 const inputTitle = document.getElementById("notetitle");
 const inputDesc = document.getElementById("noteDesc");
 const container = document.getElementById("container");
+let noteList = [];
+//make the form visible
 addBtn.addEventListener("click", () => {
     creatNotesContainer.classList.add("show");
 });
+const deletebtn = document.getElementById("eldelete");
+//make the form invisible
 cancelbtn.addEventListener("click", () => {
     creatNotesContainer.classList.remove("show");
 });
 createbtn.addEventListener("click", () => {
-    let notecontainer = document.createElement("div");
-    notecontainer.innerHTML = `
-    <h2>${inputTitle.value}</h2>
-    <p>
-      ${inputDesc.value}
-    </p>`;
-    notecontainer.classList.add("notesContainer");
-    container.appendChild(notecontainer);
-    creatNotesContainer.classList.remove("show");
-    console.log("button clicked");
+    noteList.push({
+        title: inputTitle.value,
+        desc: inputDesc.value,
+        id: noteList.length + 1,
+    });
+    updateUi();
 });
+//updates UI elements
+function updateUi() {
+    container.innerHTML = "";
+    noteList.forEach((note, index) => {
+        let notecontainer = document.createElement("div");
+        notecontainer.innerHTML = `
+    <h2>${note.title}</h2>
+    <p>
+      ${note.desc}
+    </p>
+    <span class = "deletebtn" onclick = "deleteelm(${index})">delete</span>
+    `;
+        notecontainer.classList.add("notesContainer");
+        notecontainer.setAttribute("id", `${note.id}`);
+        container.appendChild(notecontainer);
+        creatNotesContainer.classList.remove("show");
+    });
+}
+//deletes UI element
+function deleteelm(index) {
+    noteList.splice(index, 1);
+    updateUi();
+}
--- sripte.ts
+++ sripte.ts
@@ -11,22 +11,50 @@ const inputDesc: HTMLInputElement = document.getElementById(
   "noteDesc"
 )! as HTMLInputElement;
 const container: HTMLElement = document.getElementById("container")!;
+interface note {
+  title: string;
+  desc: string;
+  id: number;
+}
+let noteList: Array<note> = [];
+//make the form visible
 addBtn.addEventListener("click", () => {
   creatNotesContainer.classList.add("show");
 });
+const deletebtn: HTMLElement | null = document.getElementById("eldelete");
+//make the form invisible
 
 cancelbtn.addEventListener("click", () => {
   creatNotesContainer.classList.remove("show");
 });
 createbtn.addEventListener("click", () => {
-  let notecontainer = document.createElement("div");
-  notecontainer.innerHTML = `
-    <h2>${inputTitle.value}</h2>
-    <p>
-      ${inputDesc.value}
-    </p>`;
-  notecontainer.classList.add("notesContainer");
-  container.appendChild(notecontainer);
-  creatNotesContainer.classList.remove("show");
-  console.log("button clicked");
+  noteList.push({
+    title: inputTitle.value,
+    desc: inputDesc.value,
+    id: noteList.length + 1,
+  });
+  updateUi();
 });
+//updates UI elements
+function updateUi() {
+  container.innerHTML = "";
+  noteList.forEach((note, index) => {
+    let notecontainer = document.createElement("div");
+    notecontainer.innerHTML = `
+    <h2>${note.title}</h2>
+    <p>
+      ${note.desc}
+    </p>
+    <span class = "deletebtn" onclick = "deleteelm(${index})">delete</span>
+    `;
+    notecontainer.classList.add("notesContainer");
+    notecontainer.setAttribute("id", `${note.id}`);
+    container.appendChild(notecontainer);
+    creatNotesContainer.classList.remove("show");
+  });
+}
+//deletes UI element
+function deleteelm(index: number) {
+  noteList.splice(index, 1);
+  updateUi();
+}
